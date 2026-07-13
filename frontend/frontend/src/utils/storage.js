fe3eba68cdfcf1fc5924484bbb8378ea6b65238a
lazana-stha <lazana.stha@gmail.com>
2026-07-13 10:16:13 +0545

    Backend Integrated, Debugged

--- frontend/frontend/src/utils/storage.js
+++
@@ -1,9 +0,0 @@
-export const saveTransactions = (transactions) => {
-  localStorage.setItem("transactions", JSON.stringify(transactions));
-};
-
-// Get transactions from localStorage
-export const getTransactions = () => {
-  const data = localStorage.getItem("transactions");
-  return data ? JSON.parse(data) : [];
-};
